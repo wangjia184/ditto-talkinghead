d627195460032e33ca0ed909f3cbc20a4f49ac2e
jerry.wang <jerry.wang@everymatrix.com>
2025-03-25 15:02:58 +0800

    remove audio2motion

--- core/atomic_components/audio2motion.py
+++ core/atomic_components/audio2motion.py
@@ -72,7 +72,7 @@ class Audio2Motion:
     def setup(
         self, 
         x_s_info, 
-        overlap_v2=10,
+        overlap_v2=70,
         fix_kp_cond=0,
         fix_kp_cond_dim=None,
         sampling_timesteps=50,
@@ -80,22 +80,19 @@ class Audio2Motion:
         v_min_max_for_clip=None,
         smo_k_d=3,
     ):
-        self.smo_k_d = smo_k_d
-        self.overlap_v2 = overlap_v2
-        self.seq_frames = self.lmdm.seq_frames
-        self.valid_clip_len = self.seq_frames - self.overlap_v2
+        self.smo_k_d = smo_k_d  #3
+        self.overlap_v2 = overlap_v2 #70
+        self.seq_frames = 80
+        self.valid_clip_len = self.seq_frames - self.overlap_v2 #10
 
-        # for fuse
         self.online_mode = online_mode
-        if self.online_mode:
-            self.fuse_length = min(self.overlap_v2, self.valid_clip_len)
-        else:
-            self.fuse_length = self.overlap_v2
+
+        self.fuse_length = min(self.overlap_v2, self.valid_clip_len) #10
         self.fuse_alpha = np.arange(self.fuse_length, dtype=np.float32).reshape(1, -1, 1) / self.fuse_length
 
-        self.fix_kp_cond = fix_kp_cond
-        self.fix_kp_cond_dim = fix_kp_cond_dim
-        self.sampling_timesteps = sampling_timesteps
+        self.fix_kp_cond = fix_kp_cond  #1
+        self.fix_kp_cond_dim = fix_kp_cond_dim # [0, 202]
+        self.sampling_timesteps = sampling_timesteps #10
         
         self.v_min_max_for_clip = v_min_max_for_clip
         if self.v_min_max_for_clip is not None:
@@ -110,20 +107,26 @@ class Audio2Motion:
 
         self.clip_idx = 0
 
+
+        print("overlap_v2 : ", overlap_v2 )
+        print("smo_k_d : ", self.smo_k_d)
+        print("seq_frames : ", self.seq_frames)
+        print("fix_kp_cond : ", self.fix_kp_cond)
+        print("fix_kp_cond_dim : ", self.fix_kp_cond_dim)
+        print("v_min_max_for_clip : ", self.v_min_max_for_clip)
+        print("fuse_length : ", self.fuse_length )
+        print("fuse_alpha : ", self.fuse_alpha )
+        print("valid_clip_len : ", self.valid_clip_len )
+
     def _fuse(self, res_kp_seq, pred_kp_seq):
         ## ========================
-        ## offline fuse mode
-        ## last clip:  -------
-        ## fuse part:    *****
-        ## curr clip:    -------
-        ## output:       ^^
-        #
         ## online fuse mode
         ## last clip:  -------
         ## fuse part:       **
         ## curr clip:    -------
         ## output:          ^^
         ## ========================
+        #out = a2h.fuse( res_kp_seq, pred_kp_seq, self.seq_frames, self.valid_clip_len, self.fuse_length)
 
         fuse_r1_s = res_kp_seq.shape[1] - self.fuse_length
         fuse_r1_e = res_kp_seq.shape[1]
@@ -137,23 +140,27 @@ class Audio2Motion:
         res_kp_seq[:, fuse_r1_s:fuse_r1_e] = r_fuse    # fuse last
         res_kp_seq = np.concatenate([res_kp_seq, pred_kp_seq[:, fuse_r2_e:]], 1)  # len(res_kp_seq) + valid_clip_len
 
+        #if out.shape == res_kp_seq.shape:
+        #    print( np.mean((res_kp_seq - out) ** 2))
+        #else:
+        #    print( res_kp_seq.shape, out.shape )
+
         return res_kp_seq
     
     def _update_kp_cond(self, res_kp_seq, idx):
-        if self.fix_kp_cond == 0:  # 不重置
+        if self.clip_idx % self.fix_kp_cond == 0:  # self.fix_kp_cond == 1 here
+            self.kp_cond = self.s_kp_cond.copy()  # 重置所有
+            if self.fix_kp_cond_dim is not None:
+                ds, de = self.fix_kp_cond_dim
+                self.kp_cond[:, ds:de] = res_kp_seq[:, idx-1, ds:de]
+        else:
             self.kp_cond = res_kp_seq[:, idx-1]
-        elif self.fix_kp_cond > 0:
-            if self.clip_idx % self.fix_kp_cond == 0:  # 重置
-                self.kp_cond = self.s_kp_cond.copy()  # 重置所有
-                if self.fix_kp_cond_dim is not None:
-                    ds, de = self.fix_kp_cond_dim
-                    self.kp_cond[:, ds:de] = res_kp_seq[:, idx-1, ds:de]
-            else:
-                self.kp_cond = res_kp_seq[:, idx-1]
+            
 
     def _smo(self, res_kp_seq, s, e):
         if self.smo_k_d <= 1:
             return res_kp_seq
+        
         new_res_kp_seq = res_kp_seq.copy()
         n = res_kp_seq.shape[1]
         half_k = self.smo_k_d // 2
@@ -161,27 +168,34 @@ class Audio2Motion:
             ss = max(0, i - half_k)
             ee = min(n, i + half_k + 1)
             res_kp_seq[:, i, :202] = np.mean(new_res_kp_seq[:, ss:ee, :202], axis=1)
+
+    
+            
         return res_kp_seq
     
     def __call__(self, aud_cond, res_kp_seq=None):
         """
         aud_cond: (1, seq_frames, dim)
         """
-
+        """
         #pred_kp_seq = self.lmdm(self.kp_cond, aud_cond, self.sampling_timesteps)
         assert self.sampling_timesteps == 10
         pred_kp_seq = a2h.predict_motion(self.kp_cond, aud_cond)
         if res_kp_seq is None:
             res_kp_seq = pred_kp_seq   # [1, seq_frames, dim]
-            res_kp_seq = self._smo(res_kp_seq, 0, res_kp_seq.shape[1])
+            res_kp_seq =  a2h.moving_average_smooth(res_kp_seq, 0, res_kp_seq.shape[1], self.smo_k_d)
         else:
-            res_kp_seq = self._fuse(res_kp_seq, pred_kp_seq)  # len(res_kp_seq) + valid_clip_len
-            res_kp_seq = self._smo(res_kp_seq, res_kp_seq.shape[1] - self.valid_clip_len - self.fuse_length, res_kp_seq.shape[1] - self.valid_clip_len + 1)
+            # len(res_kp_seq) + valid_clip_len
+            res_kp_seq = a2h.fuse(res_kp_seq, pred_kp_seq, self.valid_clip_len, self.fuse_length)
+            res_kp_seq =  a2h.moving_average_smooth(res_kp_seq, res_kp_seq.shape[1] - self.valid_clip_len - self.fuse_length, res_kp_seq.shape[1] - self.valid_clip_len + 1, self.smo_k_d)
+        """
+        if res_kp_seq is None:
+            res_kp_seq = np.zeros((1, 0, 265), dtype=np.float32) 
+        res_kp_seq = a2h.audio2motion( self.s_kp_cond, np.ascontiguousarray(aud_cond), np.ascontiguousarray(res_kp_seq) )
+        #self.clip_idx += 1
 
-        self.clip_idx += 1
-
-        idx = res_kp_seq.shape[1] - self.overlap_v2
-        self._update_kp_cond(res_kp_seq, idx)
+        #idx = res_kp_seq.shape[1] - self.overlap_v2
+        #self._update_kp_cond(res_kp_seq, idx)
 
         return res_kp_seq
     
