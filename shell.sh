212b05ef9657d12393d3127e1e1230c8d07961e2
jerry.wang <vcer@qq.com>
2026-02-22 08:39:52 +0800

    udpate shell.sh

--- shell.sh
+++ shell.sh
@@ -3,5 +3,6 @@
 docker run -it --rm --runtime=nvidia --gpus all \
     --name audio2head-trainer \
     -v $(pwd):/data \
-    -v /mnt/z/HDTF_processed:/hdtf_data:ro \
+    -v /mnt/z/HDTF_processed:/data/HDTF_processed:ro \
+    -v /mnt/z/hallo3_processed:/data/hallo3_processed:ro \
     everymatrix.jfrog.io/emlab-docker/ayida/audio2head:trainer /bin/bash
